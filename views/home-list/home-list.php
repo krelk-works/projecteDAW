c6b8300d1dcfeb95ba1bece2b0381b3c0b42f9d9
Leo <leo.craciun97@gmail.com>
2024-09-22 17:32:24 +0200

    Mejora lógica PHP

--- views/home-list/home-list.php
+++ views/home-list/home-list.php
@@ -7,10 +7,10 @@
             </div>
             <div class="home-list-item-info">
                 <h3>Dona Ajaguda</h3>
-                <p>Apel·les Fenosa</p>
-                <!--<p>Localització: Primera planta</p>
-                <p>Any: 1960</p>-->
-                <p>Estat: Museu</p>
+                <p><i class="fa-solid fa-user"></i>Apel·les Fenosa</p>
+                <p><i class="fa-solid fa-location-dot"></i> Primera planta</p>
+                <p><i class="fa-solid fa-bookmark"></i> 1960</p>
+                <p><i class="fa-regular fa-clipboard"></i> Museu</p>
             </div>
         </div>
         <div class="home-list-item">
@@ -19,10 +19,10 @@
             </div>
             <div class="home-list-item-info">
                 <h3>Aire</h3>
-                <p>Apel·les Fenosa</p>
-                <!--<p>Localització: Primera planta</p>
-                <p>Any: 1960</p>-->
-                <p>Estat: Museu</p>
+                <p><i class="fa-solid fa-user"></i>Apel·les Fenosa</p>
+                <p><i class="fa-solid fa-location-dot"></i> Primera planta</p>
+                <p><i class="fa-solid fa-bookmark"></i> 1960</p>
+                <p><i class="fa-regular fa-clipboard"></i> Museu</p>
             </div>
         </div>
         <div class="home-list-item">
@@ -31,10 +31,10 @@
             </div>
             <div class="home-list-item-info">
                 <h3>Els Amants</h3>
-                <p>Apel·les Fenosa</p>
-                <!--<p>Localització: Primera planta</p>
-                <p>Any: 1960</p>-->
-                <p>Estat: Museu</p>
+                <p><i class="fa-solid fa-user"></i>Apel·les Fenosa</p>
+                <p><i class="fa-solid fa-location-dot"></i> Primera planta</p>
+                <p><i class="fa-solid fa-bookmark"></i> 1960</p>
+                <p><i class="fa-regular fa-clipboard"></i> Museu</p>
             </div>
         </div>
         <div class="home-list-item">
@@ -43,10 +43,10 @@
             </div>
             <div class="home-list-item-info">
                 <h3>Centauro</h3>
-                <p>Apel·les Fenosa</p>
-                <!--<p>Localització: Primera planta</p>
-                <p>Any: 1960</p>-->
-                <p>Estat: Museu</p>
+                <p><i class="fa-solid fa-user"></i>Apel·les Fenosa</p>
+                <p><i class="fa-solid fa-location-dot"></i> Primera planta</p>
+                <p><i class="fa-solid fa-bookmark"></i> 1960</p>
+                <p><i class="fa-regular fa-clipboard"></i> Museu</p>
             </div>
         </div>
         <div class="home-list-item">
@@ -55,10 +55,10 @@
             </div>
             <div class="home-list-item-info">
                 <h3>L'Arbre</h3>
-                <p>Apel·les Fenosa</p>
-                <!--<p>Localització: Primera planta</p>
-                <p>Any: 1960</p>-->
-                <p>Estat: Museu</p>
+                <p><i class="fa-solid fa-user"></i>Apel·les Fenosa</p>
+                <p><i class="fa-solid fa-location-dot"></i> Primera planta</p>
+                <p><i class="fa-solid fa-bookmark"></i> 1960</p>
+                <p><i class="fa-regular fa-clipboard"></i> Museu</p>
             </div>
         </div>
         <div class="home-list-item">
@@ -67,10 +67,10 @@
             </div>
             <div class="home-list-item-info">
                 <h3>Cristo de l'Agoni</h3>
-                <p>Apel·les Fenosa</p>
-                <!--<p>Localització: Primera planta</p>
-                <p>Any: 1960</p>-->
-                <p>Estat: Museu</p>
+                <p><i class="fa-solid fa-user"></i>Apel·les Fenosa</p>
+                <p><i class="fa-solid fa-location-dot"></i> Primera planta</p>
+                <p><i class="fa-solid fa-bookmark"></i> 1960</p>
+                <p><i class="fa-regular fa-clipboard"></i> Museu</p>
             </div>
         </div>
     </div>
