64958a7ec142a0fbc2deb39da6d246b9339c7dca
MiguelAngel <miguelangel.mateos@inslapineda.cat>
2024-10-04 08:23:58 +0200

    control de errores en la BBDD

--- models/database.php
+++ models/database.php
@@ -5,11 +5,15 @@
             $dbname= "bbyirnoypbjzxaryzns9";
             $username = "ujthole5uvbkxwxc";
             $password = "IYMr7GQwI6KWjKaoAhzn";
-            // New connection to the database with PDO object.
-            $this->db = new PDO("mysql:host=$servername;dbname=$dbname",$username, $password);
-            // Set the PDO error mode to exception.
-            $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            return $this->db;
+            try {
+                // New connection to the database with PDO object.
+                $this->db = new PDO("mysql:host=$servername;dbname=$dbname",$username, $password);
+                // Set the PDO error mode to exception.
+                $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                return $this->db;
+            } catch(PDOException $e) {
+                echo "<br><h1>Error a la base de dades -> </h1><br><br><br>".$e;
+            }
         }
     }
 ?>
