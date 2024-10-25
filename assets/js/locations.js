b4afa3c214066ae000711e5c6b3c42233870a7ab
Gombi14 <eric.gasull@inslapineda.cat>
2024-10-25 11:56:14 +0200

    Pasar la variable locations de PHP a JS usando apis y la funcion fetch

--- assets/js/locations.js
+++ assets/js/locations.js
@@ -1,3 +1,11 @@
-document.addEventListener("DOMContentLoaded",()=>{
-    fetch
-})
+//creates a async function 
+async function fetchLocations() {
+    try {
+        const response = await fetch('http://localhost:8080/projecteDAW/apis/locationApi.php');
+        const data = await response.json();
+        return data
+    } catch (error) {
+        console.error('Error fetching locations:', error);
+    }
+}
+fetchLocations();
--- views/location-list/location-list.php
+++ views/location-list/location-list.php
@@ -1,5 +1,9 @@
 <?php
-
-
+   $controller=new LocationController;
+    /*$data=$controller->getLocationsJSON();*/
+    echo "
+        <script src=\"/assets/js/locations.js\"></script>
+        ";
 ?>
-<script src="/assets/js/locations.js">
+
+<script src="/assets/js/locations.js"></script>
