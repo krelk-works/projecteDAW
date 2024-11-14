e85a169426da5e708743e8346adeaad280cbcef7
marcmartin <marc.martin@inslapineda.cat>
2024-11-14 09:43:20 +0100

    Creada vista de obras con diseño para ver los div

--- views/artwork-administration/artwork-administration.php
+++ views/artwork-administration/artwork-administration.php
@@ -1,22 +1,65 @@
 <div class="container-artwork-view">
     <h1>TITULO DE LA OBRA</h1>
-    <div class="image-view-container">
-        <img class="vini-en-bikini" src="https://i.pinimg.com/736x/aa/c9/a6/aac9a69680a0fe6abbc5030994ef8ca4.jpg" alt="Imagen de la obra">
-        <img class="vini-en-bikini" src="https://i.pinimg.com/736x/aa/c9/a6/aac9a69680a0fe6abbc5030994ef8ca4.jpg" alt="Imagen de la obra">
-        <img class="vini-en-bikini" src="https://i.pinimg.com/736x/aa/c9/a6/aac9a69680a0fe6abbc5030994ef8ca4.jpg" alt="Imagen de la obra">
-    </div>
-    <hr class="separator">
     <div class="artwork-view-info">
-        <h1>Nombre de la obra</h1>
-        <p>Descripción de la obra</p>
-        <p>Fecha de creación</p>
-        <p>Dimensiones</p>
-        <p>Material</p>
-        <p>Estilo</p>
-        <p>Localización</p>
-        <p>Exposición</p>
-        <p>Artista</p>
+        <h1>Identificació</h1>
+        <hr class="separator">
+        <p>Nº Registre</p>
+        <p>Nom objecte</p>
+        <p>Titol</p>
+        <p>Descripcio</p>
+
+        <h1>Detalls d'obra</h1>
+        <hr class="separator">
+        <div class="two-column-layout">
+            <p>Autor</p>
+            <p>Datacio</p>
+            <p>Data del registre</p>
+            <p>Data creacio</p>
+            <p>Bibliografia</p>
+        </div>
+
+        <h1>Caracterisitques d'obra</h1>
+        <hr class="separator">
+        <div class="two-column-layout">
+            <p>Alçada</p>
+            <p>Amplada</p>
+            <p>Fons</p>
+            <p>Classificacio generica</p>
+            <p>Codi material getty</p>
+            <p>Material getty</p>
+            <p>Cost</p>
+            <p>Quantitat</p>
+            <p>Material</p>
+            <p>Tecnica</p>
+            <p>Estat de conservacio</p>
+        </div>
+
+        <h1>Procedencia</h1>
+        <hr class="separator">
+        <div class="two-column-layout">
+            <p>Nom museu</p>
+            <p>Lloc origen</p>
+            <p>Coleccio procedencia</p>
+            <p>Tipus d’ingres</p>
+        </div>
+
+        <h1>Ubicacio</h1>
+        <hr class="separator">
+        <div class="two-column-layout">
+            <p>Ubicacio</p>
+            <p>Lloc d'execucio</p>
+        </div>
+
+        <h1>Altres dades</h1>
+        <hr class="separator">
+        <div class="two-column-layout">
+            <p>Tiratge</p>
+            <p>Causa de cancelacio</p>
+        </div>
+
+        <p>HISTORIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+            AAAAHISTORIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+            HISTORIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+            HISTORIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
     </div>
 </div>
-
-
