cc145ba83f32be418052a8ff305b9f2a2166ad65
marcmartin <marc.martin@inslapineda.cat>
2024-11-19 09:09:20 +0100

    Creada la vista de movement list

--- views/movement-list/movement-list.php
+++ views/movement-list/movement-list.php
@@ -1,5 +1,36 @@
 <?php
     $movements = new MovementsController();
     $data = $movements->getAllMovements();
-    print_r($data);
+
 ?>
+
+<main class="list-wrapper">
+    <div class="list-container list-container-javascript">
+        <div class="list-header list-header-exposition-admin">
+            <a href="">
+                <h4>Nom de la obra</h4>
+            </a>
+            <a href="">
+                <h4>Data inici</h4>
+            </a>
+            <a href="">
+                <h4>Data finzalitzacio</h4>
+            </a>
+            <a href="">
+                <h4>Destí del moviment</h4>
+            </a>
+        </div>
+        <?php
+            $MovementsController = new MovementsController();
+            $datamov = $MovementsController->getAllMovements();
+            foreach ($datamov as $data) {
+                echo '<div class="list-item list-item-exposition-admin">
+                    <h3>' . $data['name'] . '</h3>
+                    <p>' . $data['start_date'] . '</p>
+                    <p>' . $data['end_date'] . '</p>
+                    <p>' . $data['place'] . '</p>
+                </div>';
+            }
+        ?>
+    </div>
+</main>
