141ee320548cc9fe256e6f2c0966d47759a3aff1
Leo <leo.craciun97@gmail.com>
2024-11-27 10:01:35 +0100

    Mas modulo de js para la eleccion de filtro de años

--- index.php
+++ index.php
@@ -90,6 +90,7 @@
     <!-- <link rel="stylesheet" href="assets/css/chosen.min.css"> -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/chosen/1.8.7/chosen.min.css">
     <link rel="stylesheet" href="assets/css/duDatepicker.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <!-- <script src="assets/js/chosen.jquery.min.js"></script> -->
@@ -97,6 +98,8 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/chosen/1.8.7/chosen.jquery.min.js" integrity="sha512-rMGGF4wg1R73ehtnxXBt5mbUfN9JUJwbk21KMlnLZDJh7BkPmeovBuddZCENJddHYYMkCh9hPFnPmS9sspki8g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="assets/js/accordion-element.js" defer></script>
     <script src="assets/js/jstree.min.js" defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+
     
 
     <!--<script src="assets/js/main.js" defer></script>-->
