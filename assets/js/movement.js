2f7917e70914d83ce485f742efe8c73cbc681d70
Leo <leo.craciun97@gmail.com>
2024-12-11 12:12:35 +0100

    Paul

--- assets/js/movement.js
+++ assets/js/movement.js
@@ -47,28 +47,9 @@ function saveMovement(data) {
               const responseData = JSON.parse(responseText); // Parse the response text as JSON
               Swal.fire('Guardado', 'El movimiento se ha actualizado correctamente', 'success')
                       .then(() => window.location.reload());
-            //   if (responseData.success) {
-            //       Swal.fire('Guardado', 'El movimiento se ha actualizado correctamente', 'success')
-            //           .then(() => window.location.reload());
-            //   } else {
-            //       Swal.fire('Error', responseData.message, 'error');
-            //   }
           } catch (error) {
               console.error('Error parsing JSON:', error);
               Swal.fire('Error', 'Error en la respuesta del servidor', 'error');
           }
       });
 }
-
-// // Test function to simulate editing a movement
-// function testEditMovement() {
-//     const testId = 1;
-//     const testStartDate = '2023-01-01';
-//     const testEndDate = '2023-01-10';
-//     const testPlace = 'Test Place';
-
-//     showMovementDetails(testId, testStartDate, testEndDate, testPlace);
-// }
-
-// // Call the test function to simulate the edit
-// testEditMovement();
