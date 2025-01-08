f679a31922974ba6ccb0c98b71a307c4ed394ab9
MiguelAngel <miguelangel.mateos@inslapineda.cat>
2025-01-08 10:21:29 +0100

    qwertyuiopasdfghjklñçzxcvbnm

--- models/docx.php
+++ models/docx.php
@@ -5,15 +5,20 @@ require 'vendor/autoload.php';
 use PhpOffice\PhpWord\TemplateProcessor;
 
 class DOCX extends Database {
-    public function exportDOCX() {
+    public function exportDOCX($id) {
 
         $conn = $this->connect();
         $sql = "SELECT artworks.title, artworks.id_letter, artworks.id_num1, artworks.id_num2, 
-        artworks.height, artworks.width, artworks.depth, authors.name
+        artworks.height, artworks.width, artworks.depth, authors.name, materials.text AS material,
+        datations.text AS datation
         FROM artworks
         INNER JOIN authors ON artworks.author = authors.id
-        WHERE artworks.id = 342";
+        INNER JOIN materials ON artworks.material = materials.id
+        INNER JOIN datations ON artworks.datation = datations.id
+        WHERE artworks.id = :id";
+
         $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':id', $id, PDO::PARAM_STR);
         $stmt->execute();
         $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
@@ -25,8 +30,8 @@ class DOCX extends Database {
             'text2' => $result[0]['title'],
             'text3' => $result[0]['name'],
             'text4' => $result[0]['height'] . "cm/" . $result[0]['width'] . "cm/" . $result[0]['depth'] . "cm",
-            'text5' => 'Metal y madera',
-            'text6' => 'Siglo XXI',
+            'text5' => $result[0]['material'],
+            'text6' => $result[0]['datation'],
         ];
 
         foreach ($data as $placeholder => $value) {
